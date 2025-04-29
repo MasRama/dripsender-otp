b306c3341ed47593bb5e681664ec87b427c89699
MasRama <ryushineo@gmail.com>
2025-04-29 13:50:35 +0700

    change props name and add custom title props

--- src/main.js
+++ src/main.js
@@ -2,15 +2,17 @@ import './app.css'
 import App from './App.svelte'
 
 const target = document.getElementById('otp-widget')
-const vp_url = target.getAttribute('vp_url')
-const votp_url = target.getAttribute('votp_url')
+const vp_url = target.getAttribute('reqotp')
+const votp_url = target.getAttribute('verifyotp')
+const title = target.getAttribute('title') || 'Verifikasi OTP'
 
 console.log(vp_url, votp_url)
 const app = new App({
   target,
   props: {
     vp_url,
-    votp_url
+    votp_url,
+    title
   }
 
 })
